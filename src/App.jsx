a7bc8d4fa4a4efc39b924257a30da9196776c2f7
Denine <denineguy@gmail.com>
2023-01-30 20:41:52 -0500

    generating a random number for the die

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,28 @@ import Die from './Die'
 
 
 export default function App() {
-  const value = Math.floor(Math.random() * 6) + 1
+  function allNewDice() {
+    const newArray = [];
+    for(let i = 0; i < 10; i++) {
+        newArray.push(Math.floor(Math.random() * 6) + 1)
+    }
+    return newArray;
+}
+console.log(allNewDice())
   return (
     <div>
         <main>
             <div className="die--container">
-                <Die value={value}/>
-                <Die value={value}/>
-                <Die value={value}/>
-                <Die value={value}/>
-                <Die value={value}/>
-                <Die value={value}/>
-                <Die value={value}/>
-                <Die value={value}/>
-                <Die value={value}/>
-                <Die value={value}/>
+                <Die value="1"/>
+                <Die value="2"/>
+                <Die value="3"/>
+                <Die value="4"/>
+                <Die value="5"/>
+                <Die value="6"/>
+                <Die value="1"/>
+                <Die value="2"/>
+                <Die value="3"/>
+                <Die value="4"/>
             </div>
         </main>
     </div>
