7f1eb9e1aef6d722c7b9025ea242d163e07bad3a
Denine <denineguy@gmail.com>
2023-01-30 20:31:13 -0500

    adding die components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Die from './Die'
 
 
 export default function App() {
+  const value = Math.floor(Math.random() * 6) + 1
   return (
     <div>
         <main>
+            <div className="die--container">
+                <Die value={value}/>
+                <Die value={value}/>
+                <Die value={value}/>
+                <Die value={value}/>
+                <Die value={value}/>
+                <Die value={value}/>
+                <Die value={value}/>
+                <Die value={value}/>
+                <Die value={value}/>
+                <Die value={value}/>
+            </div>
         </main>
     </div>
   )
