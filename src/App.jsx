2232289bd50d8c98a1fd34b3564e26946a58097f
Denine <denineguy@gmail.com>
2023-02-04 09:11:19 -0500

    Add functionality to not generate new die for the dice that are held

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,18 @@ import { nanoid } from 'nanoid'
 export default function App() {
   const [dice, setDice] = React.useState(allNewDice())
 
+  function generateNewDie() {
+    return {
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
   function allNewDice() {
     const newDice = [];
     for(let i = 0; i < 10; i++) {
-        newDice.push({
-          value: Math.floor(Math.random() * 6) + 1,
-          isHeld: false,
-          id: nanoid()
-        })
+        newDice.push(generateNewDie())
     }
     return newDice;
   }
@@ -35,17 +39,22 @@ export default function App() {
   ))
 
   function rollDice() {
-    setDice(allNewDice())
+    // setDice(allNewDice())
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ? die : generateNewDie()
+    }))
   }
 
   return (
     <div>
-        <main>
-            <div className="die--container">
-                {diceElements}
-            </div>
-            <button className="die--roll" onClick={rollDice}>Roll</button>
-        </main>
+      <main>
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <div className="die--container">
+          {diceElements}
+        </div>
+        <button className="die--roll" onClick={rollDice}>Roll</button>
+      </main>
     </div>
   )
 }
