6eace16d32ec4a801ed95742503a683245b39f6e
Denine <denineguy@gmail.com>
2023-03-07 22:11:29 -0500

    add in logic for the counter with seconds minutes and hours

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
   const [dice, setDice] = React.useState(allNewDice())
   const [tenzies, setTenzies] = React.useState(false)
   const [count, setCount] = React.useState(0)
-  const [seconds, setSeconds] = React.useState(0)
+  const [timer, setTimer] = React.useState(0)
   const [isActive, setIsActive] = React.useState(false)
 
   React.useEffect(() => {
@@ -25,16 +25,16 @@ export default function App() {
 
     if(isActive) {
       interval = setInterval(() => {
-        setSeconds(seconds + 1);        
+        setTimer(timer + 1);        
       }, 1000);
-    } else if(!isActive && seconds !== 0) {
+    } else if(!isActive && timer !== 0) {
       clearInterval(!interval);
     }
 
     return () => clearInterval(interval);
-  }, [isActive, seconds])
+  }, [isActive, timer])
 
-  const dieText = !tenzies && !isActive ? "Start Game" : "Roll Dice"
+  const dieText = !tenzies && !isActive ? "Start Game" : "Roll Dice";
 
   function generateNewDie() {
     return {
@@ -60,6 +60,15 @@ export default function App() {
     }))
   }
 
+  const formatTime = () => {
+    const getSeconds = `0${(timer % 60)}`.slice(-2)
+    const minutes = `${Math.floor(timer / 60)}`
+    const getMinutes = `0${minutes % 60}`.slice(-2)
+    const getHours = `0${Math.floor(timer / 3600)}`.slice(-2)
+
+    return `${getHours} : ${getMinutes} : ${getSeconds}`
+  }
+
   const diceElements = dice.map(die => (
   <Die 
     key={die.id}
@@ -74,7 +83,7 @@ export default function App() {
       setDice(allNewDice)
       setCount(0)
       setIsActive(true)
-      setSeconds(0)
+      setTimer(0)
     } else {
       setCount(count + 1)
       setDice(oldDice => oldDice.map(die => {
@@ -100,23 +109,9 @@ export default function App() {
             {tenzies ? "New Game" : dieText}
         </button>
         <p className="die--roll_count_title">Roll Count: {count}</p>
-        <p>{isActive ? "true" : "false"}</p>
-        <div>{seconds}</div>
-        {/* <div className="die--roll_count">
-          <span className={`die--counter`}>{count}</span>
-        </div> */}
+        <div>{formatTime()}</div>
+        {/* <p>{isActive ? "true" : "false"}</p> */}
       </main>
     </div>
   )
 }
-
-// set the timer to 00:00
-// set the innerHTML for each setting second to 0, minute to 0 and hour to 0
-//
-
-
-//Figure out how to start the game do I need two buttons or just one button
-// if tenzies is false and isActive is false then start came
-
-//if you reach 60seconds set it back to zero and start counting up on minutes
-// use same logic for hours
